75244f56a2f1a5ffee6c92854a13e97123de0ddd
Jerome Kieffer <jerome.kieffer@esrf.fr>
2012-12-06 09:29:44 +0100

    added configuration in conf file

--- bioSaxsv1/plugins/EDPluginBioSaxsISPyBv1_0.py
+++ bioSaxsv1/plugins/EDPluginBioSaxsISPyBv1_0.py
@@ -8,6 +8,7 @@
 #    Copyright (C) 2012 ESRF
 #
 #    Principal author:        Al. de Maria
+#                             Jerome Kieffer
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -41,17 +42,26 @@ from XSDataCommon           import XSDataInteger, XSDataDouble, XSDataString, XS
 
 
 class EDPluginBioSaxsISPyBv1_0(EDPluginControl):
-    """
-	This URL will change.... where is the best place??
-    """
-    URL = "http://pcantolinos:8080/ispyb-ejb3/ispybWS/ToolsForBiosaxsWebService?wsdl"
+
+    URL = None
+    CONF_URL_KEY = "url"
+    CONF_URL_DEFAULT = "http://pcantolinos:8080/ispyb-ejb3/ispybWS/ToolsForBiosaxsWebService?wsdl"
+
 
     def __init__(self):
         """
         """
         EDPluginControl.__init__(self)
         self.setXSDataInputClass(XSDataInputBioSaxsISPyBv1_0)
-        
+        self.dataBioSaxsSample = None
+        self.dataAutoRg = None
+        self.dataGnom = None
+
+        #Params to be sent and I dont know them
+        self.volume = None
+        self.framesAverage = None
+        self.framesMerged = None
+
 
     def checkParameters(self):
         """
@@ -63,34 +73,50 @@ class EDPluginBioSaxsISPyBv1_0(EDPluginControl):
 
     def configure(self):
         """
-        Configures the ISPyB webservice access
+        Configures the ISPyB webservice access with the following parameters:
+         - The "url" key from config file        
         """
         EDPluginControl.configure(self)
-        self.DEBUG("EDPluginBioSaxsISPyBv1_0.configure")
-        self.URL        = self.URL
-        self.user       = self.dataInput.sample.getLogin().getValue() 
-        self.password   = self.dataInput.sample.getPasswd().getValue()  
+        if self.URL is None:
+            self.DEBUG("EDPluginBioSaxsISPyBv1_0.configure")
+            url = self.config.get(self.CONF_URL_KEY, None)
+            if url:
+                self.__class__.URL = url
+            else:
+                self.__class__.URL = self.CONF_URL_DEFAULT
 
 
-    
     def preProcess(self, _edObject=None):
         EDPluginControl.preProcess(self)
-        
+
         #Initializing webservices
         self.DEBUG("EDPluginBioSaxsISPyBv1_0.preProcess")
+        self.dataBioSaxsSample = self.dataInput.sample
+        user = None
+        password = ""
+        if self.dataBioSaxsSample:
+            if self.dataBioSaxsSample.login:
+                user = self.dataBioSaxsSample.login.value
+                password = self.dataBioSaxsSample.passwd.value
+        if not user:
+            self.ERROR("No login/password information in sample configuration. Giving up.")
+            self.setFailure()
+            return
+
         #I don't trust in this authentication....
-        self.httpAuthenticatedToolsForBiosaxsWebService = HttpAuthenticated(username = self.user, password = self.password)
-        self.client = Client(self.URL, transport = self.httpAuthenticatedToolsForBiosaxsWebService, cache = None)
-        
-        # XSDataBioSaxsSample
-        self.dataBioSaxsSample = self.dataInput.sample #self.getBiosaxsSample()
+
+        self.httpAuthenticatedToolsForBiosaxsWebService = HttpAuthenticated(username=user, password=password)
+        self.client = Client(self.URL, transport=self.httpAuthenticatedToolsForBiosaxsWebService, cache=None)
+
         self.dataAutoRg = self.dataInput.autoRg
         self.dataGnom = self.dataInput.gnom
-         
+
         #Params to be sent and I dont know them
-        self.volume = self.dataInput.volume.getValue()
-        self.framesAverage = self.dataInput.frameAverage.getValue()
-        self.framesMerged = self.dataInput.frameMerged.getValue()
+        if self.dataInput.volume:
+            self.volume = self.dataInput.volume.value
+
+        self.framesAverage = self.dataInput.frameAverage.value
+        self.framesMerged = self.dataInput.frameMerged.value
 
 
     def process(self, _edObject=None):
@@ -98,23 +124,23 @@ class EDPluginBioSaxsISPyBv1_0(EDPluginControl):
         self.DEBUG("EDPluginBioSaxsISPyBv1_0.process")
         try:
             self.client.service.storeDataAnalysisResultByMeasurementId(
-                                    self.dataBioSaxsSample.getMeasurementID().getValue(),
-                                    self.dataAutoRg.getFilename().getPath().getValue(),
-                                    self.dataAutoRg.getRg().getValue(),
-                                    self.dataAutoRg.getRgStdev().getValue(),
-                                    self.dataAutoRg.getI0().getValue(),
-                                    self.dataAutoRg.getI0Stdev().getValue(),
-                                    self.dataAutoRg.getFirstPointUsed().getValue(),
-                                    self.dataAutoRg.getLastPointUsed().getValue(),
-                                    self.dataAutoRg.getQuality().getValue(),
-                                    self.dataAutoRg.getIsagregated().getValue(),
-                                    self.dataBioSaxsSample.getCode().getValue(),
-                                    self.dataBioSaxsSample.getConcentration().getValue(),
-                                    self.dataGnom.getGnomFile().getPath().getValue(),
-                                    self.dataGnom.getRgGuinier().getValue(),
-                                    self.dataGnom.getRgGnom().getValue(),
-                                    self.dataGnom.getDmax().getValue(),
-                                    self.dataGnom.getTotal().getValue(),
+                                    self.dataBioSaxsSample.measurementID.value,
+                                    self.dataAutoRg.filename.path.value,
+                                    self.dataAutoRg.rg.value,
+                                    self.dataAutoRg.rgStdev.value,
+                                    self.dataAutoRg.i0.value,
+                                    self.dataAutoRg.i0Stdev.value,
+                                    self.dataAutoRg.firstPointUsed.value,
+                                    self.dataAutoRg.lastPointUsed.value,
+                                    self.dataAutoRg.quality.value,
+                                    self.dataAutoRg.isagregated.value,
+                                    self.dataBioSaxsSample.code.value,
+                                    self.dataBioSaxsSample.concentration.value,
+                                    self.dataGnom.gnomFile.path.value,
+                                    self.dataGnom.rgGuinier.value,
+                                    self.dataGnom.rgGnom.value,
+                                    self.dataGnom.dmax.value,
+                                    self.dataGnom.total.value,
                                     self.volume,
                                     self.framesAverage,
                                     self.framesMerged,
@@ -123,12 +149,12 @@ class EDPluginBioSaxsISPyBv1_0(EDPluginControl):
                                     "param3",
                                     "param4"
                                     )
-        except:
-            print "ISPyB error:"
-            self.ERROR(ValueError)
-            raise
-            
-        
+        except Exception, error:
+            strError = "ISPyB error: %s" % error
+            self.ERROR(strError)
+            self.setFailure()
+
+
 
     def postProcess(self, _edObject=None):
         EDPluginControl.postProcess(self)
