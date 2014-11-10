9f827d735865ae3f5ca7fb756c0795f2c6fd694f
Jerome Kieffer <jerome.kieffer@esrf.fr>
2014-11-10 10:39:36 +0100

    remove warning with pyFAI 0.10

--- bioSaxsv1/plugins/EDPluginBioSaxsProcessOneFilev1_3.py
+++ bioSaxsv1/plugins/EDPluginBioSaxsProcessOneFilev1_3.py
@@ -46,7 +46,7 @@ import fabio
 import numpy
 import pyFAI
 
-if pyFAI.version <= "0.8":
+if [int(i) for i in pyFAI.version.split(".")[:2]] <= [0,8]:
     EDVerbose.ERROR("Too old version of pyFAI detected ... expect to fail !")
 
 
