e1ca79da7a2fab65717ce16eef9b09710379a46b
Jerome Kieffer <jerome.kieffer@esrf.fr>
2014-11-17 17:00:07 +0100

    new plugin for HPLC (lock for buffer creation)

---
+++ bioSaxsv1/plugins/EDPluginBioSaxsFlushHPLCv1_3.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+#
+#    Project: BioSaxs
+#             http://www.edna-site.org
+#
+#    Copyright (C) 2012 ESRF
+#
+#    Principal author:        Jérôme Kieffer
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import with_statement
+
+__author__ = "Jérôme Kieffer"
+__license__ = "GPLv3+"
+__copyright__ = "2012 ESRF"
+__date__ = "2014-09-22"
+__status__ = "development"
+
+
+from EDPluginBioSaxsFlushHPLCv1_2 import EDPluginBioSaxsFlushHPLCv1_2 as EDPluginBioSaxsFlushHPLCv1_3
