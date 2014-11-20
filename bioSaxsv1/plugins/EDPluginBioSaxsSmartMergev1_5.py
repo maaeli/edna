2810a48bce68607e5a26db17c3b4d734dd76995b
Jerome Kieffer <kieffer@slavia.esrf.fr>
2014-11-20 08:55:13 +0100

    Reproduced ATSAS2.4 Behaviour in autorg. Changed SmartMerge behaviour if AutoDub fails

--- bioSaxsv1/plugins/EDPluginBioSaxsSmartMergev1_5.py
+++ bioSaxsv1/plugins/EDPluginBioSaxsSmartMergev1_5.py
@@ -526,6 +526,9 @@ class EDPluginBioSaxsSmartMergev1_5(EDPluginControl):
         self.retrieveFailureMessages(_edPlugin, "EDPluginBioSaxsSmartMergev1_5.doFailureExecAutoSub")
         self.retrieveMessages(_edPlugin)
         strErr = "Error in Processing of EDNA 'AutoSub'"
+        # clean up "EDNA memory"
+        self.__class__.lastBuffer = self.dataInput.mergedCurve
+        self.__class__.lastSample = None
         self.ERROR(strErr)
         self.addExecutiveSummaryLine(strErr)
         self.setFailure()
