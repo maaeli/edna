16dc7a07c385490eb0ce5937b55900d41984d3c7
Jerome Kieffer <jerome.kieffer@esrf.fr>
2016-06-28 15:53:03 +0200

    Commit before working on HPLC log

--- bioSaxsv1/bin/reprocess_normal.py
+++ bioSaxsv1/bin/reprocess_normal.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
                       action="store_true", dest="verbose", default=False,
                       help="switch to debug mode")
     parser.add_option("-p", "--plugin", action="store", type="string",
-                      dest="plugin", default="EDPluginBioSaxsProcessOneFilev1_3",
+                      dest="plugin", default="EDPluginBioSaxsProcessOneFilev1_4",
                       help="use an alternative plugin")
     parser.add_option("-y", "--yappi", action="store_true",
                       dest="yappi", default=False,
